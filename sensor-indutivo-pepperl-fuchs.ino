b554cf98e878aa34e81fabaeca401987893d7505
sgmtec1 <sandra.miguel3@gmail.com>
2020-12-13 02:54:58 -0300

    Add files via upload

---
+++ sensor-indutivo-pepperl-fuchs.ino
@@ -0,0 +1,46 @@
+// Open Source
+// ligação do display SDA = A4 , SLC= A5, GND, VCC;
+// ligação do Sensor pino 13 sinal (Preto) VCC= + (Marrom) GND= - (Azul)
+
+#include <Wire.h> //INCLUSÃO DE BIBLIOTECA
+#include <LiquidCrystal_I2C.h> //INCLUSÃO DE BIBLIOTECA
+
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE); //ENDEREÇO DO I2C E DEMAIS INFORMAÇÕES
+
+int sinal=13;
+int sensor;
+int i = 0;
+int conta=0;
+
+void setup() {
+
+lcd.begin(16, 2);
+pinMode (sinal,INPUT);
+ 
+
+}
+
+void loop() {
+  
+lcd.setCursor(0,0);
+lcd.print("CONTADOR DE PECA");
+lcd.setCursor(0, 1);
+lcd.print("PECA:");
+lcd.setCursor(6, 1);
+lcd.print(i);
+
+sensor = digitalRead(sinal);  
+
+while (sensor == LOW)
+{
+sensor = digitalRead(sinal); 
+}
+ 
+
+while (sensor == HIGH)
+{
+sensor = digitalRead(sinal); 
+}
+
+i++;
+}
